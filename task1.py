90acd7dc739b664594a847094ddfebe9375b3397
4LG0R <crlsnyaga@gmail.com>
2018-07-30 18:43:59 +0300

    task1&2 solutions

--- task1.py
+++ task1.py
@@ -1,9 +1,15 @@
 import random 
 
 ourList = list()
+belowFive = list()
 count = 0 
 while (count < 11):
     ourList.append(random.randint(1,10))
     count += 1
     
-ourList
+for val in ourList:
+	if (val < 5 ):
+		belowFive.append(val)
+    
+print(ourList)
+print(belowFive)
--- task2.py
+++ task2.py
@@ -2,5 +2,14 @@ import random
 
 words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
+reversePhrase = ""
 
-randomPhrase
+n = len(randomPhrase)-1
+while n >= 0: 
+	reversePhrase += randomPhrase[n]
+	n-=1
+	
+
+
+print(randomPhrase)
+print(reversePhrase)
