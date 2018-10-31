e105aefb249e271fc96631d66672ef413d9b8ca2
VinneyJ <39193976+VinneyJ@users.noreply.github.com>
2018-10-31 16:34:40 +0300

    Update task1.py
    added the comment showing what the append command does to the while loop.

--- task1.py
+++ task1.py
@@ -2,6 +2,7 @@ import random
 
 ourList = list()
 count = 0 
+#the append command adds the specified number to the list/when increasing the number
 while (count < 11):
     ourList.append(random.randint(1,10))
     count += 1
